8514c43484dddb6c8da82c74114fdb69472e0540
bsarabeswaran <bsarabeswaran@gmail.com>
2022-07-14 12:17:49 -0700

    Fixed style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
